0fa112724c0cd55431dac9c2e6a484565fdb4ca9
Will_Mackey <61839728+bobmcbobpng@users.noreply.github.com>
2021-04-29 09:50:39 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,29 @@
+let last = 0
+basic.forever(function () {
+    while (true) {
+        last = 24
+        for (let index = 0; index < 24; index++) {
+            for (let index = 0; index <= last; index++) {
+                led.plot(index % 5, index / 5)
+                basic.pause(10)
+                if (index < last) {
+                    led.unplot(index % 5, index / 5)
+                } else {
+                    last = last - 1
+                }
+            }
+        }
+        last = 24
+        for (let index = 0; index < 24; index++) {
+            for (let index2 = 0; index2 <= last; index2++) {
+                led.unplot(index2 % 5, index2 / 5)
+                basic.pause(10)
+                if (index2 < last) {
+                    led.plot(index2 % 5, index2 / 5)
+                } else {
+                    last = last - 1
+                }
+            }
+        }
+    }
+})
