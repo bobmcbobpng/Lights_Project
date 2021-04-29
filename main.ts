c8b6892766b588c0e31e1e0eeba184fd2d294da8
Will_Mackey <61839728+bobmcbobpng@users.noreply.github.com>
2021-04-29 10:17:31 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,11 +5,12 @@ basic.forever(function () {
         for (let index = 0; index < 24; index++) {
             for (let index = 0; index <= last; index++) {
                 led.plot(index % 5, index / 5)
-                basic.pause(10)
+                basic.pause(50)
                 if (index < last) {
                     led.unplot(index % 5, index / 5)
                 } else {
                     last = last - 1
+                    music.playTone(175, music.beat(BeatFraction.Quarter))
                 }
             }
         }
@@ -17,11 +18,12 @@ basic.forever(function () {
         for (let index = 0; index < 24; index++) {
             for (let index2 = 0; index2 <= last; index2++) {
                 led.unplot(index2 % 5, index2 / 5)
-                basic.pause(10)
+                basic.pause(50)
                 if (index2 < last) {
                     led.plot(index2 % 5, index2 / 5)
                 } else {
                     last = last - 1
+                    music.playTone(659, music.beat(BeatFraction.Eighth))
                 }
             }
         }
